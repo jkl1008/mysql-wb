d445862f4fa76283f821fa8924d4da81cb80e8f2
jkl1008 <544314759@qq.com>
2018-12-16 14:18:51 +0800

    sql 练习提交

--- sql_test1.py
+++ sql_test1.py
@@ -1,1 +1,17 @@
 import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="123",
+    database="test_db"
+    )
+ 
+mycursor = mydb.cursor()
+
+sql = "UPDATE sites SET name = 'ZH' WHERE name = 'Zhihu'"
+mycursor.execute(sql)
+mydb.commit()
+
+
+print(mycursor.rowcount, " 条记录被修改")
