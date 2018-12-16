69e5b338edb129e51daacae83ae973e5776bc67d
jkl1008 <544314759@qq.com>
2018-12-16 14:26:59 +0800

    sql 最后提交

--- sql_test1.py
+++ sql_test1.py
@@ -10,6 +10,15 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 sql = "UPDATE sites SET name = 'ZH' WHERE name = 'Zhihu'"
+sql = "INSERT INTO sites (name, url) VALUES ("RUNOOB", "https://www.runoob.com")" # 插入数据
+sql =  "CREATE TABLE sites (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), url VARCHAR(255))" # 主键设置
+sql = "SELECT * FROM sites WHERE name ='RUNOOB'" # 条件查询
+sql = "SELECT * FROM sites ORDER BY name"# 排序查询
+sql = "DELETE FROM sites WHERE name = 'stackoverflow'" # 删除记录
+sql = "UPDATE sites SET name = 'ZH' WHERE name = 'Zhihu'" # 更新数据
+sql = "DROP TABLE IF EXISTS sites"  # 删除数据表 sites 
+
+
 mycursor.execute(sql)
 mydb.commit()
 
